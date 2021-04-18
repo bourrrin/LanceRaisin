226c6824de317ad98c5a4e14b6a7670b09cad217
bourrrin <44211311+bourrrin@users.noreply.github.com>
2021-04-18 00:22:55 -0400

    first commit

---
+++ facialRecon/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace facialRecon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCapture = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.btnRecon = new System.Windows.Forms.Button();
+            this.btnRepositioning = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCapture
+            // 
+            this.picCapture.Location = new System.Drawing.Point(12, 12);
+            this.picCapture.Name = "picCapture";
+            this.picCapture.Size = new System.Drawing.Size(616, 426);
+            this.picCapture.TabIndex = 0;
+            this.picCapture.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(665, 54);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(108, 40);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Start videoCapture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.StartCapture_Click);
+            // 
+            // btnRecon
+            // 
+            this.btnRecon.Location = new System.Drawing.Point(665, 119);
+            this.btnRecon.Name = "btnRecon";
+            this.btnRecon.Size = new System.Drawing.Size(108, 41);
+            this.btnRecon.TabIndex = 2;
+            this.btnRecon.Text = "Detect face";
+            this.btnRecon.UseVisualStyleBackColor = true;
+            this.btnRecon.Click += new System.EventHandler(this.btnDetectFace_Click);
+            // 
+            // btnRepositioning
+            // 
+            this.btnRepositioning.Location = new System.Drawing.Point(665, 194);
+            this.btnRepositioning.Name = "btnRepositioning";
+            this.btnRepositioning.Size = new System.Drawing.Size(108, 40);
+            this.btnRepositioning.TabIndex = 3;
+            this.btnRepositioning.Text = "Initiate Repositioning";
+            this.btnRepositioning.UseVisualStyleBackColor = true;
+            this.btnRepositioning.Click += new System.EventHandler(this.btnRepositionning_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRepositioning);
+            this.Controls.Add(this.btnRecon);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.picCapture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCapture;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.Button btnRecon;
+        private System.Windows.Forms.Button btnRepositioning;
+    }
+}
+
---
+++ facialRecon/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using Emgu.CV.CvEnum;
+using Emgu.CV;
+using System.IO.Ports;
+
+
+namespace facialRecon
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+
+        private Capture cap = null;
+        private Image<Bgr, Byte> currentFrame = null;
+        Mat frame = new Mat();
+        public Point facePos;
+        static SerialPort port;
+
+        private bool facesDetectionEnable = false;
+        private bool repositionningEnable = false;
+        CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartCapture_Click(object sender, EventArgs e)
+        {
+            cap = new Capture();
+            cap.ImageGrabbed += ProcessFrame;
+            cap.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            cap.Retrieve(frame, 0);
+            currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCapture.Width, picCapture.Height,Inter.Cubic);
+
+            if(facesDetectionEnable)
+            {
+                Mat grayImage = new Mat();
+                CvInvoke.CvtColor(currentFrame, grayImage, ColorConversion.Bgr2Gray);
+                CvInvoke.EqualizeHist(grayImage, grayImage);
+                Rectangle[] faces = cascadeClassifier.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+
+                Size s = grayImage.Size;
+                facePos = new Point(s.Width / 2, s.Height / 2);
+
+                if (faces.Length > 0)
+                {
+                    foreach(var face in faces)
+                    {
+                        CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Blue).MCvScalar, 2);
+
+                        Console.WriteLine(" FacePos: " + facePos.x + " "+ facePos.y);
+                        
+                    }
+                }
+            }
+
+            picCapture.Image = currentFrame.Bitmap;
+        }
+
+        private void btnDetectFace_Click(object sender, EventArgs e)
+        {
+            facesDetectionEnable = true;
+        }
+
+        private void btnRepositionning_Click(object sender, EventArgs e)
+        {
+            repositionningEnable = true;
+
+        }
+    }
+}
